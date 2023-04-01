e0758f682b9792a5b104cb48792dec114fa2a8d6
Diego Lao Tebar <howarto@ymail.com>
2023-04-01 19:55:40 +0000

    Remove commetn

--- TypeScript/app/GildedRose.ts
+++ TypeScript/app/GildedRose.ts
@@ -9,53 +9,6 @@ export class GildedRose {
 
   updateQuality() {
     this.itemArray.forEach( item => item.update() )
-
-    // for (let i = 0; i < this.itemArray.length; i++) {
-    //   if (this.itemArray[i].name != 'Aged Brie' && this.itemArray[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-    //     if (this.itemArray[i].quality > 0) {
-    //       if (this.itemArray[i].name != 'Sulfuras, Hand of Ragnaros') {
-    //         this.itemArray[i].quality = this.itemArray[i].quality - 1
-    //       }
-    //     }
-    //   } else {
-    //     if (this.itemArray[i].quality < 50) {
-    //       this.itemArray[i].quality = this.itemArray[i].quality + 1
-    //       if (this.itemArray[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-    //         if (this.itemArray[i].sellIn < 11) {
-    //           if (this.itemArray[i].quality < 50) {
-    //             this.itemArray[i].quality = this.itemArray[i].quality + 1
-    //           }
-    //         }
-    //         if (this.itemArray[i].sellIn < 6) {
-    //           if (this.itemArray[i].quality < 50) {
-    //             this.itemArray[i].quality = this.itemArray[i].quality + 1
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (this.itemArray[i].name != 'Sulfuras, Hand of Ragnaros') {
-    //     this.itemArray[i].sellIn = this.itemArray[i].sellIn - 1;
-    //   }
-    //   if (this.itemArray[i].sellIn < 0) {
-    //     if (this.itemArray[i].name != 'Aged Brie') {
-    //       if (this.itemArray[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-    //         if (this.itemArray[i].quality > 0) {
-    //           if (this.itemArray[i].name != 'Sulfuras, Hand of Ragnaros') {
-    //             this.itemArray[i].quality = this.itemArray[i].quality - 1
-    //           }
-    //         }
-    //       } else {
-    //         this.itemArray[i].quality = this.itemArray[i].quality - this.itemArray[i].quality
-    //       }
-    //     } else {
-    //       if (this.itemArray[i].quality < 50) {
-    //         this.itemArray[i].quality = this.itemArray[i].quality + 1
-    //       }
-    //     }
-    //   }
-    // }
-
     return this.itemArray;
   }
 }
