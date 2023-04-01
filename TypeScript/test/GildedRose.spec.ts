0db9d307b181fb2569a03ade951ec81da9eb68dd
Diego Lao Tebar <howarto@ymail.com>
2023-04-01 20:09:42 +0000

    Rename file and test the second method signature to update quality

--- TypeScript/test/GildedRose.spec.ts
+++ TypeScript/test/GildedRose.spec.ts
@@ -1,7 +1,7 @@
 import { GildedRose } from '@/GildedRose'
 import { ItemFactory } from '@/ItemFactory';
 
-describe('Gilded Rose', () => {
+describe('GildedRose', () => {
 
   it( 'updates quality', () => {
     const gildedRose = new GildedRose([ ItemFactory( 'item', 5, 0 ) ])
@@ -9,6 +9,12 @@ describe('Gilded Rose', () => {
     expect( item.name ).toBe( 'item' )
     expect( item.sellIn ).toBe( 4 )
     expect( item.quality ).toBe( 0 )
+  } )
+
+  it( 'updates quality with the second method signature', () => {
+    const gildedRose = new GildedRose()
+    const itemArray = gildedRose.updateQuality()
+    expect( itemArray.length ).toBe( 0 )
   } )
 
 });
